9a12e34d98de2c98781dde6e162d24b83a810a6f
Vishal Sharma <vishal.sharma@privafy.com>
2024-10-11 21:19:32 +0530

    FoodHeaven

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -1,9 +1,21 @@
-export default ItemsList = (prop) =>{
-    const {items} = prop
-    console.log(items)
-    return(
-        <div>
-            
-        </div>
+export default ItemsList = ({prop}) => {
+    console.log(prop)
+    return (
+        <>
+            <div className="flex justify-between p-3">
+                <div className="text-left p-1 max-w-xl">
+                    <p className="font-medium">{prop?.info?.name }</p>
+                    <p className="font-medium">{prop?.info?.price ? prop?.info?.price/100 : prop?.info?.defaultPrice/100}</p>
+                    <p className="text-zinc-600 mt-10">{prop?.info?.description}</p>
+                </div>
+                <div>
+                    <img className= "rounded-md w-[150px] h-[150px]"src={"https://media-assets.swiggy.com/swiggy/image/upload/" + prop.info.imageId}/>
+                    <button className="bg-white text-green-900 rounded-xl border-2 w-24 h-10 relative bottom-6">ADD</button>
+                    <p className="font-light text-xs relative bottom-5 text-gray-500">Customiseable</p>
+                </div>
+            </div>
+            <hr/>
+        </>
+
     )
 }
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,26 +1,33 @@
 import { useParams} from 'react-router-dom'
 import useRestaurantMenu from '../utils/useRestaurantMenu'
 import { IoMdArrowDropdown } from "react-icons/io";
+import {useState} from 'react'
 import ItemsList from './ItemsList';
 
 
 export default RestaurantMenu = () => {
     const param = useParams()
     const data = useRestaurantMenu(param.resid)  //custom hook
+    const [showItems, setShowItems] = useState(false)
 
+    const handleListView = () => {
+        setShowItems(!showItems)
+    }
     if (data == null)
         return <></>
-    const menu = data[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((category) => {return category.card.card.itemCards != null})
+    
+    console.log(data)
+    const menu = data[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((category) => {return category.card.card["@type"] == "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"})
     return (
         <div className="menu-ctr mx-auto w-6/12 text-center">
             <h2 className="text-3xl font-extrabold">{data[2]?.card?.card.info.name}</h2>
             {menu && menu.map((categ, idx) =>(                        
                 <div key={idx}>
                     <div className="border-solid border-2 shadow-lg my-4 p-3 flex justify-between">
-                        <span className="font-semibold text-xl">{categ?.card?.card?.title + "(" + categ?.card?.card?.itemCards.length + ")"}</span> <span className="text-xl font-semibold mt-2"> <IoMdArrowDropdown /> </span>
+                        <span className="font-medium text-lg">{categ?.card?.card?.title + "(" + categ?.card?.card?.itemCards.length + ")"}</span> <span className="text-xl font-semibold mt-2" onclick={handleListView}> <IoMdArrowDropdown /> </span>
                     </div>
                     {categ?.card?.card?.itemCards?.map((menuItem) => (
-                            <ItemsList key={menuItem.card.info.id} prop={menuItem?.card}/>
+                            showItems ? <ItemsList key={menuItem.card.info.id} prop={menuItem?.card}/>  : <></>
                         ))
                     }
                 </div>
